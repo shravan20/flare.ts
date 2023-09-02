55e72a8d0d39fe2676ba613ca15aae8e2bee0214
S K B <shravankumarb@locus.sh>
2023-09-02 13:50:13 +0530

    :rotating_light: Apply linter and pre commit hooks
    Apply linter and pre commit hooks

---
+++ . prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true, // Add semicolons at the end of statements
+  singleQuote: true, // Use single quotes for strings
+  trailingComma: "all", // Add trailing commas wherever applicable (e.g., in arrays and objects)
+  tabWidth: 2, // Set the tab width to 2 spaces
+  printWidth: 80, // Specify the maximum line length
+  arrowParens: "always", // Always include parentheses around arrow function parameters
+  endOfLine: "auto", // Use the operating system's line-ending style (LF on macOS/Linux, CRLF on Windows)
+};
