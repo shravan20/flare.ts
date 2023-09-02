d42dd4f4667a57e15cf1c37ee75f6e7b1a210950
S K <47430686+shravan20@users.noreply.github.com>
2023-09-02 13:14:52 +0530

    Added sample test cases (#7)

---
+++ tests/test.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+
+describe("Sample Test Suite", () => {
+  it("Sample Test Case-1", () => {
+    expect(true).to.be.true;
+  });
+
+  it("Sample Test Case-2", () => {
+    expect(false).to.be.false;
+  });
+
+  // Add more test cases as needed
+});
