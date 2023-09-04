4431e4d182c85e3ba83b806ff592223d2d8f9220
Kartikeya Manchanda <96577422+kartikeyam1@users.noreply.github.com>
2023-09-04 13:50:51 +0530

    Update sample.ts

--- lib/sample.ts
+++ lib/sample.ts
@@ -2,3 +2,7 @@ import { isAnyPropertyTrue } from "./utility/ArrayUtils/index";
 
 const flag = isAnyPropertyTrue([1, 3, 4, 5, 66], (value) => value % 2 === 0);
 console.log(flag);
+
+
+const flag2 = isAnyPropertyTrue(["hell", "hello", "buffalo", "yellow", "zoo"], (value) => value.endsWith("o"));
+console.log(flag2);
