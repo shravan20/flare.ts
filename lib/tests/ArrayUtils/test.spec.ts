3a52ae3c585417ff9c4b20de920dc3a6dc94f349
Shravan <shravankumarb@locus.sh>
2023-10-04 04:29:31 +0530

    feat: Added array utils

--- lib/tests/ArrayUtils/test.spec.ts
+++ lib/tests/ArrayUtils/test.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { fillArray, filterFalsy } from "../../utility/ArrayUtils/index";
+import { fillArray, filter, filterFalsy, filterNegate } from "../../utility/ArrayUtils/index";
 
 describe("Sample Test Suite", () => {
   it("Sample Test Case-1", () => {
@@ -42,5 +42,20 @@ describe("filterFalsy", () => {
     // Empty list, so the result should also be an empty list
     expect(result).to.be.an("array");
     expect(result).to.have.lengthOf(0);
+  });
+});
+
+describe("Filter Functions", () => {
+  const numbers: number[] = [1, 2, 3, 4, 5];
+  it("should filter even numbers", () => {
+    const isEven = (num: number): boolean => num % 2 === 0;
+    const evenNumbers = filter(numbers, isEven);
+    expect(evenNumbers).to.deep.equal([2, 4]);
+  });
+
+  it("should filterNegate even numbers to get odd numbers", () => {
+    const isEven = (num: number): boolean => num % 2 === 0;
+    const oddNumbers = filterNegate(numbers, isEven);
+    expect(oddNumbers).to.deep.equal([1, 3, 5]);
   });
 });
--- lib/utility/ArrayUtils/index.ts
+++ lib/utility/ArrayUtils/index.ts
@@ -15,3 +15,14 @@ export function fillArray<T>(length: number, value: T): T[] {
 export function filterFalsy<T>(list: T[]): T[] {
   return list.filter((item) => item);
 }
+
+export function filter<T>(list: T[], predicate: Predicate<T>): T[] {
+  return list.filter(predicate);
+}
+
+export function filterNegate<T>(list: T[], predicate: Predicate<T>): T[] {
+  return filter(list, item => !predicate(item));
+}
+
+// TODO: To be moved to Monad Utils
+type Predicate<T> = (item: T) => boolean;
