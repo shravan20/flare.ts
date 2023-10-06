b27156c7e1b281224f8240fbd7439da61dee48de
Shravan <shravankumarb@locus.sh>
2023-10-07 01:39:24 +0530

    feat: Release as NPM package

--- lib/sample.ts
+++ lib/sample.ts
@@ -3,6 +3,8 @@ import { isAnyPropertyTrue } from "./utility/ArrayUtils/index";
 const flag = isAnyPropertyTrue([1, 3, 4, 5, 66], (value) => value % 2 === 0);
 console.log(flag);
 
-
-const flag2 = isAnyPropertyTrue(["hell", "hello", "buffalo", "yellow", "zoo"], (value) => value.endsWith("o"));
+const flag2 = isAnyPropertyTrue(
+  ["hell", "hello", "buffalo", "yellow", "zoo"],
+  (value) => value.endsWith("o"),
+);
 console.log(flag2);
